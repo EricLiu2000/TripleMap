8f12ce8a7b4e9d9d3129902217183a77d5339f17
Eric Liu <ericxliu@gmail.com>
2018-11-27 01:23:43 -0500

    Implemented event creation and saving functionality

--- app/src/main/java/edu/umich/triplemap/EventActivity.java
+++ app/src/main/java/edu/umich/triplemap/EventActivity.java
@@ -1,7 +1,11 @@
 package edu.umich.triplemap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+    }
+
+    public void saveEvent(View view) {
+        Intent intent = new Intent(this, ScheduleActivity.class);
+        intent.putExtra("eventFrequency", ((Switch) findViewById(R.id.eventFrequency)).isChecked());
+        intent.putExtra("eventName", ((EditText) findViewById(R.id.eventName)).getText().toString());
+        intent.putExtra("eventAddress", ((EditText) findViewById(R.id.eventAddress)).getText().toString());
+        intent.putExtra("eventRoom", ((EditText) findViewById(R.id.eventRoom)).getText().toString());
+        intent.putExtra("eventDate", ((EditText) findViewById(R.id.eventDate)).getText().toString());
+        intent.putExtra("eventStartTime", ((EditText) findViewById(R.id.eventStartTime)).getText().toString());
+        startActivity(intent);
+    }
+
+    public void cancelChanges(View view) {
+
+    }
+
+    public void deleteEvent(View view) {
+
     }
 }
--- app/src/main/java/edu/umich/triplemap/ScheduleActivity.java
+++ app/src/main/java/edu/umich/triplemap/ScheduleActivity.java
@@ -1,13 +1,70 @@
 package edu.umich.triplemap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class ScheduleActivity extends AppCompatActivity {
+
+    private HashMap<String, String[]> events = new HashMap<String, String[]>();
+
+    public HashMap<String, String[]> getEvents() {
+        return events;
+    }
+
+    Spinner spinner;
+    ArrayAdapter<String> dataAdapter;
+
+    private void recordEvent() {
+        Intent intent = getIntent();
+
+        String[] details = new String[6];
+        details[0] = intent.getStringExtra("eventFrequency");
+        details[1] = intent.getStringExtra("eventName");
+        details[2] = intent.getStringExtra("eventAddress");
+        details[3] = intent.getStringExtra("eventRoom");
+        details[4] = intent.getStringExtra("eventDate");
+        details[5] = intent.getStringExtra("eventStartTime");
+
+        events.put(intent.getStringExtra("eventName"), details);
+    }
+
+    public void createNewEvent(View view) {
+        Intent intent = new Intent(this, EventActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        ArrayList<String> initialMessage = new ArrayList();
+        initialMessage.add("Please create an event");
+        spinner  = findViewById(R.id.spinner2);
+        dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, initialMessage);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setContentView(R.layout.activity_schedule);
+        setIntent(intent);
+        recordEvent();
+
+        spinner = findViewById(R.id.spinner2);
+        dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new ArrayList<>(events.keySet()));
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 }
